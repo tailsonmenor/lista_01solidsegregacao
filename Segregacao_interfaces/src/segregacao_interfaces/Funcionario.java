bb5bdb81e640e7470c3f80ac6412cf3a46071cb5
tailsonmenor <50224821+tailsonmenor@users.noreply.github.com>
2021-06-25 19:45:26 -0300

    Add files via upload

---
+++ Segregacao_interfaces/src/segregacao_interfaces/Funcionario.java
@@ -0,0 +1,36 @@
+
+package segregacao_interfaces;
+
+
+public class Funcionario {
+    public void calcularSalarioParaGerente(Gerente gerente){
+
+	}
+
+public void calcularSalarioParaCoordenador(Coordenador coordenador){
+
+	}
+
+public void calcularSalarioParaOperador(Operador operador){
+
+}
+
+    private static class Coordenador {
+
+        public Coordenador() {
+        }
+    }
+
+    private static class Gerente {
+
+        public Gerente() {
+        }
+    }
+
+    private static class Operador {
+
+        public Operador() {
+        }
+    }
+
+}
---
+++ Segregacao_interfaces/src/segregacao_interfaces/Segregacao_interfaces.java
@@ -0,0 +1,12 @@
+
+package segregacao_interfaces;
+
+
+public class Segregacao_interfaces {
+
+    
+    public static void main(String[] args) {
+    
+    
+}
+}
